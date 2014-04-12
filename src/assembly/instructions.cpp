8702f4da1b4d548430e5decc42949e13e5f14a8a
Adam Schwalm <adamschwalm@gmail.com>
2014-04-11 23:14:55 -0500

    Now locates 'retn' instructions

--- src/assembly/instructions.cpp
+++ src/assembly/instructions.cpp
@@ -5,6 +5,46 @@ namespace assembly
 {
     const std::set<Op_t> opcodes
     {
-        /*PUSH EAX*/{0x50}
+        {0xC3}, //ret
+        {0xFF, 0xE0}, //jmp eax
+        {0xFF, 0xE3}, //jmp ebx
+        {0xFF, 0xE1}, //jmp ecx
+        {0xFF, 0xE2}, //jmp edx
+        {0xFF, 0xE7}, //jmp edi
+        {0xFF, 0xE6}, //jmp esi
+        {0xFF, 0xE4}, //jmp esp
+        {0xFF, 0xE5}, //jmp ebp
+        {0xFF, 0x20}, //jmp [eax]
+        {0xFF, 0x23}, //jmp [ebx]
+        {0xFF, 0x21}, //jmp [ecx]
+        {0xFF, 0x22}, //jmp [edx]
+        {0xFF, 0x27}, //jmp [edi]
+        {0xFF, 0x26}, //jmp [esi]
+        {0xFF, 0x24, 0x24}, //jmp [esp]
+        {0xFF, 0x65, 0x00}, //jmp [ebp]
+        {0x50}, //push eax
+        {0x53}, //push ebx
+        {0x51}, //push ecx
+        {0x52}, //push edx
+        {0x57}, //push edi
+        {0x56}, //push esi
+        {0x54}, //push esp
+        {0x55}, //push ebp
+        {0xFF, 0x30}, //push [eax]
+        {0xFF, 0x33}, //push [ebx]
+        {0xFF, 0x31}, //push [ecx]
+        {0xFF, 0x32}, //push [edx]
+        {0xFF, 0x37}, //push [edi]
+        {0xFF, 0x36}, //push [esi]
+        {0xFF, 0x34, 0x24}, //push [esp]
+        {0xFF, 0x75, 0x00}, //push [ebp]
+        {0x58}, //pop eax
+        {0x5B}, //pop ebx
+        {0x59}, //pop ecx
+        {0x5A}, //pop edx
+        {0x5F}, //pop edi
+        {0x5E}, //pop esi
+        {0x5C}, //pop esp
+        {0x5D}, //pop ebp
     };
 }
--- src/rapport.cpp
+++ src/rapport.cpp
@@ -2,14 +2,32 @@
 #include "assembly/instructions.hpp"
 #include <boost/program_options.hpp>
 #include <iostream>
+#include <fstream>
+#include <algorithm>
 
 namespace options = boost::program_options;
+
+std::vector<uint8_t> readBytes(std::string filename)
+{
+    using namespace std;
+
+    ifstream ifs(filename, ios::binary | ios::ate);
+    auto pos = ifs.tellg();
+
+    vector<uint8_t> result(pos);
+
+    ifs.seekg(0, ios::beg);
+    auto start = &result[0];
+    ifs.read(reinterpret_cast<char*>(start), pos);
+
+    return result;
+}
 
 int main(int argc, char *argv[]) {
     options::options_description desc("Allowed options");
     desc.add_options()
         ("help", "produce help message")
-        ("pad,p", options::value<int>(), "pad output to fill buffer")
+        ("pad,p", options::value<int>()->default_value(0), "pad output to fill buffer")
         ("file", options::value<std::string>()->required(), "input file")
         ;
 
@@ -22,10 +40,20 @@ int main(int argc, char *argv[]) {
                    .positional(positionalOptions).run(),
                    vm);
 
-    options::notify(vm);
-
+    // Help check before notify to prevent exception if requesting help with no file
     if (vm.count("help")) {
         std::cout << desc << "\n";
         return 0;
+    }
+
+    options::notify(vm);
+
+    auto contents = readBytes(vm["file"].as<std::string>());
+
+    for (auto i = contents.begin(); i != contents.end(); ++i) {
+        i = std::find(i, contents.end(), 0xC3);
+        if (i == contents.end()) {break;}
+        std::cout << std::hex << "0x"
+                  << std::distance(contents.begin(), i) << std::endl;
     }
 }
